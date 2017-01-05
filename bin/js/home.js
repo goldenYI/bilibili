ade5ed29b94e331623ece5640e4ba68be1448110
梦靡 <1310451979@qq.com>
2017-01-05 17:14:00 +0800

    project init

---
+++ bin/js/home.js
@@ -0,0 +1,1 @@
+webpackJsonp([0],[function(n,s){"use strict";a?b:c}]);
---
+++ dist/js/home.js
@@ -0,0 +1,11 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ function(module, exports) {
+
+	"use strict";
+	
+	var d = a ? b : c;
+
+/***/ }
+]);
+//# sourceMappingURL=home.js.map
